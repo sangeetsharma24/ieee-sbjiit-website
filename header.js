56cb25c5f2556b4d6e77a3ae1a47878ac18f3218
1012Sahil <91882375+1012Sahil@users.noreply.github.com>
2022-09-04 00:19:34 +0530

    Update header.js
    Now the navbar closes when the contact and home links are clicked

--- header.js
+++ header.js
@@ -1,9 +1,17 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
+const navbarHomeLink = document.querySelector('.home');
+const navbarContactUsLink = document.querySelector('.contact');
 
 const toggleNavbar = () => {
-  // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
+function closeMobileNavbar() {
+  nav_header.classList.remove('active');
+}
+
+
 mobile_nav.addEventListener("click", () => toggleNavbar());
+navbarContactUsLink.addEventListener("click", closeMobileNavbar);
+navbarHomeLink.addEventListener("click", closeMobileNavbar);
